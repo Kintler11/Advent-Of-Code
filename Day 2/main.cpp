d45e6d1fe00e401da1a2b6c3ec486225506e3146
Kintler11 <tomsonkait@gmail.com>
2024-12-04 18:08:51 +0200

    Clean up code

--- Day 2/main.cpp
+++ Day 2/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 const int SAFE_RANGE = 3;
+
+bool isSafe(vector<int> numbers) {
+    bool isDecreasing = numbers[1] - numbers[0] < 0;
+    int lastNumber = numbers[0];
+    
+    for (int i = 1; i < numbers.size(); i++) {
+        int difference = lastNumber - numbers[i];
+
+        bool isSafe = (
+            difference != 0 && 
+            abs(difference) <= SAFE_RANGE &&
+            ((difference < 0 && !isDecreasing) || (difference > 0 && isDecreasing))
+        );
+
+        if (!isSafe) {
+            return false;
+        }
+        
+        lastNumber = numbers[i];
+    }
+
+    return true;
+}
 
 int main(int argc, char const *argv[]) {
     int safeReportCount = 0;
@@ -14,44 +38,16 @@ int main(int argc, char const *argv[]) {
     if (myfile.is_open()) {
         while(getline(myfile, line)) {
             stringstream ss(line); 
-            bool hasChecked = false;
-            bool isDecreasing = NULL;
-            int lastNumber = NULL;
-
+            vector<int> lineNumbers;
             int number;
-            bool isSafe = true;
-
+            
             while (ss >> number) {
-                if (!isSafe) {
-                    break;
-                }
-                if (lastNumber == NULL) {
-                    lastNumber = number;
-                    continue;
-                }
-
-                if (!hasChecked) {
-                    isDecreasing = lastNumber > number;
-                    hasChecked = true;
-                }
-
-                int difference = lastNumber - number;
-
-                isSafe = (
-                    difference != 0 && 
-                    abs(difference) <= SAFE_RANGE &&
-                    ((difference < 0 && !isDecreasing) || (difference > 0 && isDecreasing))
-                );
-                
-                lastNumber = number;
+                lineNumbers.push_back(number);
             }
 
-            if (isSafe) {
+            if (isSafe(lineNumbers)) {
                 safeReportCount++;
             }
-
-            cout << line << endl;
-            cout << (isSafe ? "Safe" : "UnSafe") << endl;
         }
         myfile.close();
     }
