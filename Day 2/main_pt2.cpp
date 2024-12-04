8425abf31f95ef82f3557da1e2aafef896a13b0c
Kintler11 <tomsonkait@gmail.com>
2024-12-04 19:38:02 +0200

    Day 2 part 2 done

--- Day 2/main_pt2.cpp
+++ Day 2/main_pt2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 const int SAFE_RANGE = 3;
 
-bool isSafe(vector<int> numbers) {
+int isSafe(vector<int> numbers) {
     bool isDecreasing = numbers[numbers.size() - 1] - numbers[0] < 0;
     int lastNumber = numbers[0];
     
@@ -21,13 +21,13 @@ bool isSafe(vector<int> numbers) {
         );
 
         if (!isSafe) {
-            return false;
+            return i;
         }
         
         lastNumber = numbers[i];
     }
 
-    return true;
+    return 0;
 }
 
 int main(int argc, char const *argv[]) {
@@ -40,12 +40,29 @@ int main(int argc, char const *argv[]) {
             stringstream ss(line); 
             vector<int> lineNumbers;
             int number;
-            
+
             while (ss >> number) {
                 lineNumbers.push_back(number);
             }
 
-            if (isSafe(lineNumbers)) {
+            int index = isSafe(lineNumbers);
+
+            if (index == 0 ) {
+                safeReportCount++;
+                continue;
+            }
+
+            bool allowed = false;
+            vector<int> one = lineNumbers;
+
+            lineNumbers.erase(next(lineNumbers.begin(), index));
+            one.erase(next(one.begin(), index - 1));
+
+            if (isSafe(lineNumbers) == 0 || isSafe(one) == 0 ) {
+                allowed = true;
+            }
+
+            if (allowed) {
                 safeReportCount++;
             }
         }
